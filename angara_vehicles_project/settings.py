5f358051a4cbf253a02244f2af3b8643706a0066
grSashha <90776548+grSashha@users.noreply.github.com>
2023-11-13 00:30:32 +0300

    db_creation

--- angara_vehicles_project/settings.py
+++ angara_vehicles_project/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'angara_vehicles_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'angara_vehicles_assembly',
+        'USER': 'postgres',
+        'PASSWORD': 'psadeiw123',
+        'HOST': 'localhost',
+        'PORT': 5433,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
